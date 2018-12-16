a9dd4a131131830ed42edbc98b72b76ebb83946a
aaronkkim <aaron.molly.kim@gmail.com>
2018-12-16 10:33:39 -0700

    I can grab a title!

--- index.js
+++ index.js
@@ -4,25 +4,30 @@ const mysql = require ('mysql');
 const request = require('request');
 const axios = require('axios');
 
-const getMovies =  () => {
-    axios.get('https://api.themoviedb.org/3/discover/movie?api_key=4fba833d14a0e854b19fc2ba5dc2f8bc&certification_country=US&certification=R&sort_by=vote_average.desc&primary_release_date.gte=2015-01-01&primary_release_date.lte=2015-12-31&with_genres=878&sort_by=popularity.desc&limit=100')
-.then(response=> {
-    //console.log(response.data)
-   // console.log(response);
-    const results =response.data
-    //console.log(results)
-    var movieRows =[]})
-    .then(results=>{results.forEach((movie)=>{
-        console.log(movie.title+'Hi')
-    })
-    console.log(results)
-    //console.log(response.data.explanation);
+request('https://api.themoviedb.org/3/discover/movie?api_key=4fba833d14a0e854b19fc2ba5dc2f8bc&certification_country=US&certification=R&sort_by=vote_average.desc&primary_release_date.gte=2015-01-01&primary_release_date.lte=2015-12-31&with_genres=878&sort_by=popularity.desc&limit=100',{json:true},(err,res,body)=>{
+    if (err){return console.log(err);}
+    var movies = body
+    console.log(body.results[0].title);
+    console.log(body.explanation)
 })
-.catch(  (error) => {
-    const response = error.response
-    console.log(response +'error')
-  })
-  }
+//     axios.get('https://api.themoviedb.org/3/discover/movie?api_key=4fba833d14a0e854b19fc2ba5dc2f8bc&certification_country=US&certification=R&sort_by=vote_average.desc&primary_release_date.gte=2015-01-01&primary_release_date.lte=2015-12-31&with_genres=878&sort_by=popularity.desc&limit=100')
+// .then(response=> {
+//     //console.log(response.data)
+//    // console.log(response);
+//     const results =response.data
+//     //console.log(results)
+//     var movieRows =[]})
+//     .then(results=>{results.forEach((movie)=>{
+//         console.log(movie.title+'Hi')
+//     })
+//     console.log(results)
+//     //console.log(response.data.explanation);
+// })
+// .catch(  (error) => {
+//     const response = error.response
+//     console.log(response +'error')
+//   })
+  
   
 
 
@@ -70,6 +75,6 @@ app.get('/pro/add',(req, res)=>{
     })
 });
 app.listen(4000,()=>{
-    console.log('Products server listening on port 4000')
-    console.log(getMovies())
+    console.log('Movie server listening on port 4000')
+    // console.log(getMovies())
 });
