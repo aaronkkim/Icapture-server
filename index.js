5162d0716aafeb121de27ddf64c45c17e4a403dd
aaronkkim <aaron.molly.kim@gmail.com>
2018-12-16 14:42:07 -0700

    trying new look at query

--- index.js
+++ index.js
@@ -2,38 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const mysql = require ('mysql');
 const request = require('request');
-const axios = require('axios');
-
-request('https://api.themoviedb.org/3/discover/movie?api_key=4fba833d14a0e854b19fc2ba5dc2f8bc&certification_country=US&certification=R&sort_by=vote_average.desc&primary_release_date.gte=2015-01-01&primary_release_date.lte=2015-12-31&with_genres=878&sort_by=popularity.desc&limit=100',{json:true},(err,res,body)=>{
-    if (err){return console.log(err);}
-    var movies = body
-    console.log(body.results[0].title);
-    console.log(body.explanation)
-})
-//     axios.get('https://api.themoviedb.org/3/discover/movie?api_key=4fba833d14a0e854b19fc2ba5dc2f8bc&certification_country=US&certification=R&sort_by=vote_average.desc&primary_release_date.gte=2015-01-01&primary_release_date.lte=2015-12-31&with_genres=878&sort_by=popularity.desc&limit=100')
-// .then(response=> {
-//     //console.log(response.data)
-//    // console.log(response);
-//     const results =response.data
-//     //console.log(results)
-//     var movieRows =[]})
-//     .then(results=>{results.forEach((movie)=>{
-//         console.log(movie.title+'Hi')
-//     })
-//     console.log(results)
-//     //console.log(response.data.explanation);
-// })
-// .catch(  (error) => {
-//     const response = error.response
-//     console.log(response +'error')
-//   })
-  
-  
-
 
 const app = express();
-
-const SELECT_ALL_PRODUCTS_QUERY ='SELECT* FROM movies2'
+const fields = ['id', 'adult', 'backdrop_path', 'genre_ids', 'original_language', 'original_title', 'overview', 'popularity', 'poster_path', 'release_date', 'title', 'video', 'vote_average', 'vote_count'];
+const queryStringParams = fields.map(function() {
+    return '?';
+});
+const queryString = 'insert into movies3(' + fields.join() + ') values (' + queryStringParams + ');';
+console.log(queryString);
+const SELECT_ALL_MOVIES_QUERY ='SELECT* FROM movies3'
 
 const connection = mysql.createConnection({
     host: '159.89.131.130',
@@ -50,8 +27,9 @@ connection.connect(err=>{
 
 app.use(cors());
 
-app.get('8081/display',(req, res)=>{
-    connection.query(SELECT_ALL_PRODUCTS_QUERY,(err, results)=>{
+
+app.get('/display',(req, res)=>{
+    connection.query(SELECT_ALL_MOVIES_QUERY,(err, results)=>{
         if(err){
             return res.send(err)
         }
@@ -62,26 +40,39 @@ app.get('8081/display',(req, res)=>{
         }
     })
 });
-app.get('/8081/gather',(req, res)=>{
-    const{id,adult,backdrop_path,genre_ids,original_language,original_title,overview,popularity,poster_path,release_date,title,video,vote_average,vote_count} = req.query;
-    const INSERT_PRODUCTS_QUERY = `INSERT INTO movies3(id,adult,backdrop_path,genre_ids,original_language,original_title,overview,popularity,poster_path,release_date,title,video,vote_average,vote_count) 
-    VALUES('${id}','${adult}','${backdrop_path}','${genre_ids}','${original_language}','${original_title}','${overview}','${popularity}','${poster_path}','${release_date}','${title}','${video}','${vote_average}','${vote_count}');`;
-    request('https://api.themoviedb.org/3/discover/movie?api_key=4fba833d14a0e854b19fc2ba5dc2f8bc&certification_country=US&certification=R&sort_by=vote_average.desc&primary_release_date.gte=2015-01-01&primary_release_date.lte=2015-12-31&with_genres=878&sort_by=popularity.desc&limit=100',{json:true},(err,res,body)=>{
-    if (err){return console.log(err);}
-    var movies = body.results[0]
-    console.log(body.results[0].title);
-    console.log(body.explanation)
-})
-    connection.query(INSERT_PRODUCTS_QUERY,(err, movies)=>{
-        if(err){
-            return res.send(err)
-        }
-        else{
-            return res.send('successfully added')
-        }
-    })
+app.get('/gather',(req, res)=>{
+
+    request('https://api.themoviedb.org/3/discover/movie?api_key=4fba833d14a0e854b19fc2ba5dc2f8bc&certification_country=US&certification=R&sort_by=vote_average.desc&primary_release_date.gte=2015-01-01&primary_release_date.lte=2015-12-31&with_genres=878&sort_by=popularity.desc&limit=100',
+    {json: true},
+    (err, res, body)=>{
+        console.log(body.results)
+        // Loop over query results, each result is a movie
+        body.results.forEach(function(movie) {
+           
+            let queryData = fields.map(function(d) {
+                if(!movie.hasOwnProperty(d)) { // Movie didn't have the field we wanted, return as a string and hope that MySQL doesn't puke because a different column type (BUG)
+                    return '';
+                } else {
+                    if(typeof movie[d] == 'object') { // This field is a complex object, lets just return a JSON string value
+                            return JSON.stringify(movie[d]);
+                    } else {
+                            return typeof movie[d] == 'string' ? movie[d].replace(/[\u0800-\uFFFF]/g, '') : movie[d]; // Clean string of wild characters that MySQL didn't like (POSSIBLE BUG on UTF data think APAC movie titles)
+                    }
+                }
+            });
+            
+            connection.query(queryString, queryData, function(err,res) {
+
+                if(err) {
+                    console.log('error', err); // eslint-disable-line no-console
+                }
+                console.log(res)
+            });
+            
 });
+    })
+})
 app.listen(4000,()=>{
     console.log('Movie server listening on port 4000')
-    // console.log(getMovies())
 });
+
